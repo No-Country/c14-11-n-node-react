ea0b68af19c7c5e336fe228bb145ff74bf52437a
enzo2022 <you@example.com>
2023-10-11 21:40:48 -0300

    LandingPageAvance

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import {Route, Router, Routes} from 'react-router-dom'
 import { Auth } from './components/Login/Auth'
 import Card from './components/card'
+import LandingPage from './components/landing page/landing page'
+
 
 export default function App() {
   return (
     <div>
-      <Auth />
-      <Card />
+    <Routes>
+      <Route path='/' element={<><LandingPage/></>}/>
+      <Route path='/login' element={<><Auth/></>}/>
+     </Routes>
+    
+      {/* <Auth />
+      <Card /> */}
     </div>
   )
 }
