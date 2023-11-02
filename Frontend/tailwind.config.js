d568cd48322cd84badf935f420b784d194f2e499
OG0808 <ogarns@proton.me>
2023-11-02 17:43:54 -0500

    fix

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,14 +1,14 @@
-const {nextui} = require("@nextui-org/react");
+// const {nextui} = require("@nextui-org/react");
 
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
 
-export default {
-  content: ["./src/**/*.{html,js}",
-  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [nextui()],
-}
+// export default {
+//   content: ["./src/**/*.{html,js}",
+//   "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   darkMode: "class",
+//   plugins: [nextui()],
+// }
 
